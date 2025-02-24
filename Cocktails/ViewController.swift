5223f9b000c68720cee8d728097002853e1febc7
lord-of-the-lost <ignatov.99@yandex.ru>
2025-02-24 19:45:09 +0300

    Initial Commit

---
+++ Cocktails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktails
+//
+//  Created by Николай Игнатов on 24.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
